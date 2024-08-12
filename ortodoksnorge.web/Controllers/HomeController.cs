e80a9577bc8fc5d3394a96cdc1d33a6344f653a3
Owen Andrew Jørgensen <owen@jorgensenconsult.no>
2024-08-12 19:41:34 +0200

    WIP

---
+++ ortodoksnorge.web/Controllers/HomeController.cs
@@ -0,0 +1,5 @@
+﻿namespace OrtodoksNorge.Web.Controllers;
+
+public class HomeController
+{
+}
---
+++ ortodoksnorge.web/Views/ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrtodoksNorge.Web.Views
+{
+    public class ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
